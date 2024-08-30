48664db3c3b60fc738fb253ac24226ef0daaa0b2
Gordillo4045 <andre.vizuet14@gmail.com>
2024-08-30 13:37:10 -0700

    prueba

--- src/components/FooterCustom.tsx
+++ src/components/FooterCustom.tsx
@@ -1,7 +1,7 @@
 export default function () {
     return (
         <div className="flex items-center justify-center">
-            <span className="text-sm font-thin">Derechos reservados</span>
+            <span className="text-sm font-thin">Derechos reservadoss</span>
         </div>
     )
 }
