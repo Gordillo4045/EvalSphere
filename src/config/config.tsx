65a5dd6a26318c5c86194c650d708b3e0a802b33
Gordillo4045 <andre.vizuet14@gmail.com>
2024-09-01 12:45:48 -0700

    config

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -10,13 +10,13 @@ import { getFunctions as getFirebaseFunctions, httpsCallable as firebaseHttpsCal
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID
+    apiKey: "AIzaSyDK2yCLaBVxFrGSCHCJg5zFufff2VkOCS8",
+    authDomain: "eval-sphere.firebaseapp.com",
+    projectId: "eval-sphere",
+    storageBucket: "eval-sphere.appspot.com",
+    messagingSenderId: "184962243352",
+    appId: "1:184962243352:web:a751b134904a7e3c201c6d",
+    measurementId: "G-3N0YJVY1KC"
 };
 
 // Initialize Firebase
