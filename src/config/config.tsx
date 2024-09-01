7130fc37b0b7b97ca78dfda7f2c83bc615979869
Gordillo4045 <andre.vizuet14@gmail.com>
2024-09-01 12:40:30 -0700

    update

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -10,13 +10,13 @@ import { getFunctions as getFirebaseFunctions, httpsCallable as firebaseHttpsCal
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    VITE_API_KEY: string;
+    VITE_AUTH_DOMAIN: string;
+    VITE_PROJECT_ID: string;
+    VITE_STORAGE_BUCKET: string;
+    VITE_MESSAGING_SENDER_ID: string;
+    VITE_APP_ID: string;
+    VITE_MEASUREMENT_ID: string;
+}
