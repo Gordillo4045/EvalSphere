ab78ae70b2dce0041365fa25e001e90e8c3ab2df
Gordillo4045 <andre.vizuet14@gmail.com>
2024-09-17 12:08:36 -0700

    fix config

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { connectAuthEmulator, getAuth } from "firebase/auth";
-import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getFunctions as getFirebaseFunctions, httpsCallable as firebaseHttpsCallable, connectFunctionsEmulator } from "firebase/functions";
+import { getFunctions as getFirebaseFunctions, httpsCallable as firebaseHttpsCallable } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
