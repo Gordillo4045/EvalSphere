8ebda3189705ec5214ff756b55283ec67070337f
Gordillo4045 <andre.vizuet14@gmail.com>
2025-02-18 12:14:04 -0800

    Reduce height of MarqueeReview component for better layout consistency

--- src/components/MarqueeReview.tsx
+++ src/components/MarqueeReview.tsx
@@ -116,7 +116,7 @@ const ReviewCard = ({
 
 export function MarqueeReview() {
     return (
-        <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg  bg-background ">
+        <div className="relative flex h-[400px] w-full flex-col items-center justify-center overflow-hidden rounded-lg  bg-background ">
             <Marquee pauseOnHover className="[--duration:20s]">
                 {firstRow.map((review) => (
                     <ReviewCard key={review.username} {...review} />
